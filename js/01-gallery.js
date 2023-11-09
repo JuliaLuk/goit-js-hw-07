65021139aa6f19658603a0d6c64475ee75492e27
JuliaLuk <julia1lukasiewicz@gmail.com>
2023-11-09 12:49:47 -0600

    01 (dz7)

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,7 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+
+const imgGallery = document.querySelector(".gallery");
+console.log(imgGallery);
 
 console.log(galleryItems);
