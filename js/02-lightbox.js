553d11b224c8d3416fb24f4a1a1371ccf58df8f7
JuliaLuk <julia1lukasiewicz@gmail.com>
2023-11-13 10:31:31 -0600

    02 dz 7

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,43 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const imgGallery = document.querySelector(".gallery");
+// console.log(imgGallery);
+
+imgGallery.insertAdjacentHTML("beforeend", createMarkup(galleryItems));
+
+imgGallery.addEventListener("click", handelClick);
+
+function handelClick(event) {
+  if (event.target === event.currentTarget) {
+    return;
+  }
+  console.log(event.currentTarget);
+
+  const instance = basicLightbox.create(`
+  	<div class="modal">
+        <img class="gallery__link" src="${event.target.dataset.source}" alt="{description}"/>
+      </div>
+  `);
+  instance.show();
+}
+
+function createMarkup(arr) {
+  return arr
+    .map(
+      ({ preview, original, description }) => `
+  <li class="item gallery__item">
+  <img
+  class="gallery__image lazyload"
+  src="${preview}" 
+  alt="${description}" 
+  data-source = "${original}" 
+  width="300">
+  </li>`
+    )
+    .join("");
+}
+
+// console.log(createMarkup(galleryItems));
+
+// console.log(galleryItems);
